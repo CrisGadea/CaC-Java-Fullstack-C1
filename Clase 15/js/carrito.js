f21a4ced8ab06d301da95987c69a1e4c5e0adc3c
CrisGadea <cristianhernangadea@gmail.com>
2022-05-17 19:26:38 -0300

    Carrito con js

---
+++ Clase 15/js/carrito.js
@@ -0,0 +1,66 @@
+// PASOS
+
+// Obtener el evento onclick del usuario en los botones agregar al carrito
+
+// En el evento, agregar el producto seleccionado al array del carrito
+
+// Obtenemos los datos del carrito
+let carrito = [];
+
+if (localStorage.getItem("Carrito")) {
+    carrito = JSON.parse(localStorage.getItem("Carrito"));
+}
+
+const generarCards = (productos) => {
+    // Obtenemos el div que contendra las cards de productos
+    let cards = document.getElementById("carrito");
+
+    let total = 0;
+    
+    productos.forEach( producto => {
+        
+        total += producto.precio;  // total = total + producto.precio;
+
+        // Creamos la etiqueta Card
+        let cardProductos = document.createElement("div");
+        cardProductos.className = "card m-3";
+
+        let card = `
+            <img class="card-img-top h-50 w-50" src="${producto.img}" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">${producto.nombre}</h4>
+                <p class="card-text">
+                    ${producto.descripcion}
+                </p>
+                <p class="card-text">
+                    ${producto.precio}
+                </p>
+                <a class="btn btn-primary" id="cart${producto.id}">Eliminar</a>
+            </div>
+        `;
+    
+        cardProductos.innerHTML = card;
+        cards.appendChild(cardProductos);
+
+        let productCard = document.getElementById("cart" + producto.id);
+
+        productCard.addEventListener("click", (evento)=>{
+            evento.preventDefault();
+            carrito.splice(producto.id - 1, 1);
+        });
+
+
+    });
+    
+}
+
+
+generarCards(carrito);
+
+
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    localStorage.setItem("Carrito", JSON.stringify(carrito));
+});
--- Clase 15/js/index.js
+++ Clase 15/js/index.js
@@ -1,186 +1,19 @@
-/* 
----------------  Lista de productos a mostrar en la página  -------------------
-    * Array [] de productos ( de formato JSON {} )  // JSON: JavaScript Object Notation
-*/
-const productos = [
-    {
-        id: 1,
-        nombre: "Producto 1",
-        img: "./img/imagen.jpg",
-        precio: 1000,
-        descripcion: "lorem insump 1",
-        stock: 10
-    },
-    {
-        id: 2,
-        nombre: "Producto 2",
-        img: "../img/img.jpg",
-        precio: 2000,
-        descripcion: "lorem insump 2",
-        stock: 20
-    },
-    {
-        id: 3,
-        nombre: "Producto 3",
-        img: "",
-        precio: 3000,
-        descripcion: "lorem insump 3",
-        stock: 30
-    },
-    {
-        id: 4,
-        nombre: "Producto 4",
-        img: "",
-        precio: 4000,
-        descripcion: "lorem insump 4",
-        stock: 40
-    },
-    {
-        id: 5,
-        nombre: "Producto 5",
-        img: "",
-        precio: 5000,
-        descripcion: "lorem insump 5",
-        stock: 50
-    }
-];
 
-// Convertimos el array de objetos en un formato tipo JSON
-const productosEnStorage = JSON.stringify(productos);
+// let nombre = "Cristian";
 
-// Guardamos en el localstorage el array JSON convertido de productos
-localStorage.setItem("productos", productosEnStorage);
+// let apellido = "Gadea";
 
-
-let productosObtenidosDelStorage = JSON.parse(localStorage.getItem("productos"));
-
-
-/**
-     * 
-     * const card = document.createElement("div");
-    card.className = "card";
-
-
-    const parrafo = document.createElement("p");
-    parrafo.innerHTML = "Hola";
-
-    card.appendChild(parrafo);
-
-    const products = document.getElementById("products");
-
-    let texto = "${products} "
-
-    // Literales
-
-
-    const divFinal = ` <div class="card"> 
-        <p> Hola </p>
-    </div>`;
-
-    //document.body.appendChild(divFinal);
-
-
-    
-        <div class="card"> 
-          <p> Hola </p>
-         </div>
-     
-
-    products.appendChild(card);
-
-    const h3 = document.createElement("h3");
-    h3.className = "card-header";
-
-    const h4 = document.createElement("h4");
-    h4.className = "card-tittle";
-
-    const cardBody = document.createElement("div");
-    cardBody.className = "card-body";
-
-    const p = document.createElement("p");
-    p.className = "card-text";
-
-    const button = document.createElement("a");
-    button.className = "btn btn-primary";
-    button.innerText = "Add";
-    button.href = "#"; */
-
-
-
-
-
-const generarcards = (productos) => {
-    // Obtenemos el div que contendra las cards de productos
-    const cards = document.getElementById("products");
-
-    cards.classList = "container-fluid";
-
-
-    let total = 0;
-
-    productos.forEach( producto => {
-        total += producto.precio;
-
-        let cardProductos = document.createElement("div");
-        cardProductos.className = "card";
-
-        let card = `
-            <img class="card-img-top" src="${producto.img}" alt="Card image cap">
-            <div class="card-body">
-                <h4 class="card-title">${producto.nombre}</h4>
-                <p class="card-text">
-                    ${producto.descripcion}
-                </p>
-                <p class="card-text">
-                    ${producto.precio}
-                </p>
-                <a href="#!" class="btn btn-primary">Agregar al Carrito</a>
-            </div>
-        `;
-
-        cardProductos.innerHTML = card;
-
-        products.appendChild(cardProductos);
-
-        let precioTotal = document.createElement("p");
-        precioTotal.className = "text-white";
-        precioTotal.innerHTML = total;
-
-        products.appendChild(precioTotal)
-
-        console.log(total)
-
-
-    });
-
-    localStorage.setItem("precioTotal",total);
-
-    
-}
-
-
-generarcards(productos);
-
-
-
-
-
-
-let nombre = "Cristian";
-
-let apellido = "Gadea";
-
-var edad = 25;
+// var edad = 25;
 
 
 
 // Creamos una funcion que imprime por consola del navegador nombre completo,
 // recibiendo como parámetro el nombre y el apellido
-function mostrarNombreCompleto (nombre1, apellido1) {
-    var nombreCompleto = nombre1 + " " + apellido1;
+// function mostrarNombreCompleto (nombre1, apellido1) {
+//     var nombreCompleto = nombre1 + " " + apellido1;
 
-    console.log(nombreCompleto);
-}
+//     console.log(nombreCompleto);
+// }
 
 // Para ejecutar la funcion que imprime un nombre completo, la llamamos
 
@@ -190,13 +23,13 @@ function mostrarNombreCompleto (nombre1, apellido1) {
 // ------------------------------------------- //
 
 // Sobreescribimos los valores de las variables para ejecutar nuevamente la funcion
-nombre = "Jose";
+// nombre = "Jose";
 
-apellido = "Doe";
+// apellido = "Doe";
 
 //mostrarNombreCompleto(nombre, apellido);
 
-nombre = "Cristian";
+// nombre = "Cristian";
 
 
 // Estructuras
@@ -217,7 +50,125 @@ if (nombre == "Cristian" && edad >= 18) {
 
 
 // Bucle for => Repite un codigo tantas veces como le indiquemos en la validacion
-for (let numero = 1; numero <= 10; numero++) {
-    console.log(numero);
+// for (let numero = 1; numero <= 10; numero++) {
+//     console.log(numero);
+// }
+
+// Limpiamos el localStorage
+localStorage.clear();
+
+/* 
+---------------  Lista de productos a mostrar en la página  -------------------
+    * Array [] de productos ( de formato JSON {} )  // JSON: JavaScript Object Notation
+*/
+
+const productos = [
+    {
+        id: 1,
+        nombre: "Producto 1",
+        img: "./img/notebook.webp",
+        precio: 1000,
+        descripcion: "lorem insump 1",
+        stock: 10
+    },
+    {
+        id: 2,
+        nombre: "Producto 2",
+        img: "./img/notebook.webp",
+        precio: 2000,
+        descripcion: "lorem insump 2",
+        stock: 20
+    },
+    {
+        id: 3,
+        nombre: "Producto 3",
+        img: "./img/notebook.webp",
+        precio: 3000,
+        descripcion: "lorem insump 3",
+        stock: 30
+    },
+    {
+        id: 4,
+        nombre: "Producto 4",
+        img: "./img/notebook.webp",
+        precio: 4000,
+        descripcion: "lorem insump 4",
+        stock: 40
+    },
+    {
+        id: 5,
+        nombre: "Producto 5",
+        img: "./img/notebook.webp",
+        precio: 5000,
+        descripcion: "lorem insump 5",
+        stock: 50
+    }
+];
+
+// Convertimos el array de objetos en un formato tipo JSON
+const productosEnStorage = JSON.stringify(productos);
+
+// Guardamos en el localstorage el array JSON convertido de productos
+localStorage.setItem("productos", productosEnStorage);
+
+
+let productosObtenidosDelStorage = JSON.parse(localStorage.getItem("productos"));
+
+let carrito = [];
+
+const generarCards = (productos) => {
+    // Obtenemos el div que contendra las cards de productos
+    let cards = document.getElementById("products");
+
+    let total = 0;
+    productos.forEach( producto => {
+        
+        total += producto.precio;  // total = total + producto.precio;
+
+        // Creamos la etiqueta Card
+        let cardProductos = document.createElement("div");
+        cardProductos.className = "card m-3";
+
+        let card = `
+            <img class="card-img-top" src="${producto.img}" alt="Card image cap">
+            <div class="card-body">
+                <h4 class="card-title">${producto.nombre}</h4>
+                <p class="card-text">
+                    ${producto.descripcion}
+                </p>
+                <p class="card-text">
+                    ${producto.precio}
+                </p>
+                <a class="btn btn-primary" id="cart${producto.id}">Agregar al Carrito</a>
+            </div>
+        `;
+    
+        cardProductos.innerHTML = card;
+        cards.appendChild(cardProductos);
+
+        let productCard = document.getElementById("cart" + producto.id);
+
+        productCard.addEventListener("click", (evento)=>{
+            evento.preventDefault();
+            carrito.push(producto);
+        });
+
+
+    });
+
+    localStorage.setItem("precioTotal",total);
+    
 }
+
+
+generarCards(productos);
+
+
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    localStorage.setItem("Carrito", JSON.stringify(carrito));
+});
+
 
